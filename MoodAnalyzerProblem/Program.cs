7d6855f57e9043fb1be453f14ff86c430cb93da5
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-12 18:48:16 +0530

    [Madhavee] Add method to find mood with testcases

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -10,7 +10,13 @@ namespace MoodAnalyzerProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mood Analyzer");
+            Console.WriteLine("\nWelcome to Mood Analyzer");
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(); //Creating a object of MooddAnalyzer class
+            Console.WriteLine("\nEnter any message : ");
+            string message = Console.ReadLine();
+            string mood = moodAnalyzer.AnalyzeMood(message); // calling method of class with paasing user message as parameter
+            Console.WriteLine($"\n{mood} mood");
+            Console.ReadLine();
         }
     }
 }
